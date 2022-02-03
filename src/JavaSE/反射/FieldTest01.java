bf652eeba56abc32dfaceee030a87f59f49516c4
ALB <9752591+shao-mingtao@user.noreply.gitee.com>
2022-02-03 14:16:42 +0800

    reflect

---
+++ src/JavaSE/反射/FieldTest01.java
@@ -0,0 +1,21 @@
+package JavaSE.反射;
+
+import java.lang.reflect.Field;
+//获取一个指定类中的属性，
+public class FieldTest01 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class c=Class.forName("JavaSE.反射.User");
+        Field field[]=c.getFields();        //只返回public
+
+        System.out.println(field.length);
+
+        Field f=field[0];
+        System.out.println(f.getName());
+
+        Field field1[]=c.getDeclaredFields();   //返回所有属性的字段
+        System.out.println(field1.length);
+        for (Field fields:field1){
+            System.out.println(fields.getName());
+        }
+    }
+}
--- src/JavaSE/反射/User.java
+++ src/JavaSE/反射/User.java
@@ -1,7 +1,14 @@
 package JavaSE.反射;
 
 public class User {
+    public int number;
+    private String name;
+    protected String sex;
+
+
     public User(){
         System.out.println("测试一下用反射机制实例化的对象能不能输出无参构造的内容哈");
+
+
     }
 }
